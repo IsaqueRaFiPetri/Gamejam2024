76e405913696d495d564527614ff97511660b25b
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-08-08 14:36:16 -0300

    thdrer

---
+++ Gamejam 2024/Assets/GoToScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    public void Teleport(string tp)
+    {
+        SceneManager.LoadScene(tp);
+    }
